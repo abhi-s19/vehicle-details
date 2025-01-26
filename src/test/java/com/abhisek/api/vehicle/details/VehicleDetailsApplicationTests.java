7d767841fab949742a16a1f7661c677cd75d3536
Abhisek Garai <abhi.garai.2090@gmail.com>
2025-01-26 17:47:16 +0530

    initial commit

---
+++ src/test/java/com/abhisek/api/vehicle/details/VehicleDetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhisek.api.vehicle.details;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleDetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
